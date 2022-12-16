2327dcc0d6a1a45328ed62009f8d6e04e4cca1ca
Sven <sven.siegenthaler@bbz-cfp.ch>
2022-12-16 11:51:43 +0100

    changes

--- game.js
+++ game.js
@@ -1,9 +1,9 @@
 var spieler = document.querySelector(".player");
-spieler.style.left = "1000px";
-spieler.style.bottom = "10px";
+spieler.style.left = "300px";
+spieler.style.bottom = "30px";
 var timer = new Timer(20);
 var timer2 = new Timer(100);
-var timer3 = new Timer(200);
+var timer3 = new Timer(140);
 
 var jump = false;
 
@@ -23,33 +23,33 @@ function loop() {
   if (timer2.ready()) {
     var h = document.createElement("div");
     h.classList.add("hinderniss1");
-    h.style.bottom = "10px";
+    h.style.bottom = "5px";
     h.style.right = "-10px";
     spielfeld.appendChild(h);
   }
   if (timer3.ready()) {
     var h = document.createElement("div");
     h.classList.add("hinderniss2");
-    h.style.bottom = "600px";
+    h.style.bottom = "200px";
     h.style.right = "-10px";
     spielfeld.appendChild(h);
   }
 
   //background bewegung
-  backgroundPosition = backgroundPosition + 10;
+  backgroundPosition = backgroundPosition + 5;
   spielfeld.style.backgroundPosition = `-${backgroundPosition}px 0`;
 
   //bewegung game und tastatur
   if (parseInt(spieler.style.bottom) > 5) {
     spieler.style.bottom = parseInt(spieler.style.bottom) - 10 + "px";
   }
-  if (mouseClick() && parseInt(spieler.style.bottom) < 350) {
+  if (mouseClick() && parseInt(spieler.style.bottom) < 200) {
     jump = true;
     spieler.style.bottom = parseInt(spieler.style.bottom) + 25 + "px";
   } else if (jump) {
     spieler.style.bottom = parseInt(spieler.style.bottom) + 25 + "px";
   }
-  if (parseInt(spieler.style.bottom) > 450) {
+  if (parseInt(spieler.style.bottom) > 200) {
     jump = false;
   }
 
@@ -62,14 +62,14 @@ function loop() {
   //hindernisse
   var hindernisse1 = document.querySelectorAll(".hinderniss1");
   for (var hinderniss1 of hindernisse1) {
-    hinderniss1.style.right = parseInt(hinderniss1.style.right) + 20 + "px";
+    hinderniss1.style.right = parseInt(hinderniss1.style.right) + 10 + "px";
     if (parseInt(hinderniss1.style.left) < 0) {
       hinderniss1.parentNode.removeChild(hinderniss1);
     }
   }
   var hindernisse2 = document.querySelectorAll(".hinderniss2");
   for (var hinderniss2 of hindernisse2) {
-    hinderniss2.style.right = parseInt(hinderniss2.style.right) + 70 + "px";
+    hinderniss2.style.right = parseInt(hinderniss2.style.right) + 15 + "px";
     if (parseInt(hinderniss2.style.left) < 0) {
       hinderniss2.parentNode.removeChild(hinderniss2);
     }
